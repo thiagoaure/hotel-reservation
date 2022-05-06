d084359d994a942088224f80f8de4db033b18a21
thiago aureliano <thiagoh544@gmail.com>
2022-05-06 19:28:22 -0300

    feat: adiciona funcao que obtem tipo do clinte e dias da semana para hospedagem

--- src/main.js
+++ src/main.js
@@ -1,3 +1,33 @@
+const hotels = require('./data/hotels');
+
+const CheapestHotel = {
+    name: '',
+    totalPrice: 0,
+}
+
+const weekday = ['mon', 'tues', 'wed', 'thur', 'fri'];
+const weekend = ['sat', 'sun'];
+
+function countDaysOfWeek(input) {
+    let countWeekday = 0;
+    let countWeekend = 0;
+
+    const typeClient = input.split(":");
+
+    weekday.map((item, index) => {
+        if(input.includes(item)) {
+            countWeekday++;
+        }
+    })
+
+    weekend.map((item, index) => {
+        if(input.includes(item)) {
+            countWeekend++;
+        }
+    })
+    return {typeClient, countWeekday, countWeekend}
+}
+
 function getCheapestHotel (input) { //DO NOT change the function's name.
     return "Cheapest hotel name"
 }
